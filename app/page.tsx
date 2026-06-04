a27e1db62971b1d633059b78ada61cd6508cbec8
veper <veper.chesley@protonmail.com>
2026-06-04 19:02:21 +0800

    remove unused file and import

--- app/page.tsx
+++ app/page.tsx
@@ -7,19 +7,14 @@ import { MobileHeader } from "@/app/components/navigation/MobileHeader";
 import { BottomNav } from "@/app/components/navigation/BottomNav";
 import { Footer } from "@/app/components/navigation/Footer";
 import { PriceTicker } from "@/app/components/ticker/PriceTicker";
-import { TransactionWall } from "@/app/components/transactions/TransactionWall";
 import { HeroSearch } from "@/app/components/home/HeroSearch";
 import { TrustBanner } from "@/app/components/home/TrustBanner";
-import { SniperRadar } from "@/app/components/home/SniperRadar";
 import { PremiumMarket } from "@/app/components/home/PremiumMarket";
 import { PortfolioRewards } from "@/app/components/home/PortfolioRewards";
 import { NewArrivals } from "@/app/components/home/NewArrivals";
-import { TokyoMarketIndex } from "@/app/components/home/TokyoMarketIndex";
 import { WishlistTicker } from "@/app/components/shared/WishlistTicker";
 import { ExecutionSlideOver } from "./components/transactions/ExecutionSlideOver";
-import { FeaturedCarousel } from "@/app/components/home/FeaturedCarousel";
 import { CheckInCard } from "@/app/components/rewards/CheckInCard";
-import { TransactionWallSkeleton } from "@/app/components/shared/StreamingSkeletons";
 
 // 將 PwaInstallPrompt 封裝為非 SSR 的純客戶端動態組件！
 // 這樣 Server 端渲染時這裡會完全保持真空，等 Client 進場水合完畢後才加載
@@ -32,9 +27,6 @@ const PwaInstallPrompt = dynamic(
 );
 
 export default function HomePage() {
-  // TODO: [database] Replace with realtime stream connection state from Supabase channel status
-  const isTransactionWallLoading = false;
-
   return (
     <div className="min-h-[100dvh] bg-bg-page flex flex-col">
       {/* 頂部全域看盤元件列 */}
